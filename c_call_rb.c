bfd6895679ba07702edcae0f2b31681f033d8d8e
windwiny <windwiny.ubt@gmail.com>
2014-01-17 20:56:44 +0800

    pass arguments

--- c_call_rb.c
+++ c_call_rb.c
@@ -21,17 +21,31 @@ int crb_eval_string(const char *rbstr) {
 	return error;
 }
 
+
 #define MAX 4096
 char evalStr[MAX];
 char *debug_flag = "false";
+int help_flag = 0;
 
 int main(int argc, char **argv) {
+	int i,j;
 	int res;
+	crb_init();
 
-	for (res=1; res<argc; res++) {
-		if ((strcmp("--debug", argv[res])==0) || (strcmp("-d", argv[res])==0)) {
-			debug_flag = "true";
+
+	for (i=1; i < argc; i++) {
+		if (strcmp("--", argv[i])==0) {
+			VALUE vargv;
+			vargv = rb_gv_get("$*");
+			for (j = i+1; j < argc; j++)
+			{
+				rb_ary_push(vargv, rb_str_new2(argv[j]));
+			}
 			break;
+		} else if ((strcmp("--debug", argv[i])==0) || (strcmp("-d", argv[i])==0)) {
+			debug_flag = "true";
+		} else if ((strcmp("--help", argv[i])==0) || (strcmp("-h", argv[i])==0)) {
+			help_flag = 1;
 		}
 	}
 
@@ -39,6 +53,7 @@ int main(int argc, char **argv) {
 		"require 'rbconfig'\n"
 		"$DEBUG=%s\n"
 		"\n"
+		"puts %%Q{ARGV: #{ARGV}} if $DEBUG\n"
 		"$:.unshift '.'\n"
 		"puts %%Q{LOAD_PATH: #{$:}} if $DEBUG\n"
 		"argv0 = %%q*%s*\n"
@@ -49,23 +64,21 @@ int main(int argc, char **argv) {
 		"require rbfn\n"
 		,debug_flag, argv[0]);
 
-	for (res=1; res<argc; res++) {
-		if ((strcmp("--help", argv[res])==0) || (strcmp("-h", argv[res])==0)) {
-			printf(
-			"%s syntax:\n"
-			"\n"
-			"  -h  --help     show this message\n"
-			"  -d  --debug    set $DEBUG to true\n"
-			"\n"
-			"---- this program will run ----\n"
-			"%s\n"
-			"--------\n"
-			, argv[0], evalStr);
-			exit(0);
-		}
+	if (help_flag) {
+		printf(
+		"%s syntax:\n"
+		"\n"
+		"  -h  --help                  show this message\n"
+		"  -d  --debug                 set $DEBUG to true\n"
+		"  --  [arg1 [arg2 [...]]]     all arguments pass to ruby program after '--'\n"
+		"\n"
+		"---- this program will run ----\n"
+		"%s\n"
+		"--------\n"
+		, argv[0], evalStr);
+		exit(0);
 	}
 
-	crb_init();
 	res = crb_eval_string(evalStr);
 	crb_cleanup();
 
